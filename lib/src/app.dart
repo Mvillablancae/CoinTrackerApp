83ff4c2075e99eeaa50169bb489f1dc8b1fdf4ac
MVillablanca <marcelo.villablancae@sansano.usm.cl>
2023-09-23 13:03:26 -0300

    Clean Flutter Sample project

---
+++ lib/src/app.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'features/sample_feature/sample_item_details_view.dart';
+import 'features/sample_feature/sample_item_list_view.dart';
+import 'features/settings/settings_controller.dart';
+import 'features/settings/settings_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+    required this.settingsController,
+  });
+
+  final SettingsController settingsController;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          restorationScopeId: 'app',
+          theme: ThemeData(),
+          darkTheme: ThemeData.dark(),
+          themeMode: settingsController.themeMode,
+          onGenerateRoute: (RouteSettings routeSettings) {
+            return MaterialPageRoute<void>(
+              settings: routeSettings,
+              builder: (BuildContext context) {
+                switch (routeSettings.name) {
+                  case SettingsView.routeName:
+                    return SettingsView(controller: settingsController);
+                  case SampleItemDetailsView.routeName:
+                    return const SampleItemDetailsView();
+                  case SampleItemListView.routeName:
+                  default:
+                    return const SampleItemListView();
+                }
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
